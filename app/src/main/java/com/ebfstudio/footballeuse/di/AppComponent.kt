43dc18407663d1f2cea4dfa9e6ff22257f42a82f
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-09-19 18:41:26 +0200

    🏗 Dependencies injection setup

---
+++ app/src/main/java/com/ebfstudio/footballeuse/di/AppComponent.kt
@@ -0,0 +1,6 @@
+package com.ebfstudio.footballeuse.di
+
+/**
+ * Created by Vince on 19/09/2020
+ */
+val appComponent = listOf(appModule)
---
+++ app/src/main/java/com/ebfstudio/footballeuse/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.ebfstudio.footballeuse.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+/**
+ * Created by Vince on 19/09/2020
+ */
+val appModule = module {
+
+}
